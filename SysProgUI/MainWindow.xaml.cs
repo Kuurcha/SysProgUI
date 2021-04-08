00117541ec8d80ee6ae38ba5a819bc84638495c5
Kuurcha <yhyr@mail.ru>
2021-04-08 18:03:44 +0300

    Basic UI

--- SysProgUI/MainWindow.xaml.cs
+++ SysProgUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace SysProgUI
 {
@@ -22,7 +23,53 @@ namespace SysProgUI
     {
         public MainWindow()
         {
-            InitializeComponent();
+            //InitializeComponent();
+         
+            //Button btn = new Button();
+            //btn.FontWeight = FontWeights.Bold;
+
+            //WrapPanel pnl = new WrapPanel();
+
+            //TextBlock txt = new TextBlock();
+            //txt.Text = "Multi";
+            //txt.Foreground = Brushes.Blue;
+            //pnl.Children.Add(txt);
+
+            //txt = new TextBlock();
+            //txt.Text = "Color";
+            //txt.Foreground = Brushes.Red;
+            //pnl.Children.Add(txt);
+
+            //txt = new TextBlock();
+            //txt.Text = "Button";
+            //pnl.Children.Add(txt);
+
+            //btn.Content = pnl;
+            //btn.Margin = new Thickness(200, 200, 0, 0);
+            //btn.Background = Brushes.Magenta;
+            //canvas.Children.Add(btn);
+            //var fileDialog = new OpenFileDialog();
+
+        }
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }

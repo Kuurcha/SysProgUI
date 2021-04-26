5a9b712db7bddb276dde64196e63a94fea76df46
Kuurcha <yhyr@mail.ru>
2021-04-26 20:45:49 +0300

    Revert "Revert "UI-3: View and presentor for ASM added + Asm interface (not c# interface class)""
    This reverts commit abef7241f1065a2f13c9158df09dda0927d24c0a.

---
+++ SysProgUI/IView/IViewAnalyse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SysProgUI.IView
+{
+    public interface IViewAnalyse
+    {
+        string analyseLbl { set; }
+
+        string toAnalyseTB { get; }
+
+        public void ShowMessageBoxAnalyse(string message);
+
+        public event Action AsmResultRequest;
+    }
+}
---
+++ SysProgUI/IView/IViewAsmCalcResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+using System.Windows.Media;
+
+namespace SysProgUI.IView
+{
+    public interface IViewAsm
+    {
+        string asmResult { set; }
+        
+        string aValue { get; }
+        string bValue { get; }
+        bool AsmMode { get; }
+
+        public void ShowMessageBoxAsm(string message);
+
+        public event Action AsmResultRequest;
+    }
+}
+
